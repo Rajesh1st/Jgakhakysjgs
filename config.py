5382203c4e238c271b9c6736995af0ca2bdcbe88
Rajesh1st <rajeshtb55@outlook.com>
2025-03-23 11:43:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,4 +3,5 @@ import os
 API_ID = int(os.getenv("API_ID", "YOUR_API_ID"))
 API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
 BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
+ADMIN_ID = os.getenv("ADMIN_ID", "ADMIN_ID")
 MONGO_URI = os.getenv("MONGO_URI", "YOUR_MONGODB_CONNECTION_STRING")
