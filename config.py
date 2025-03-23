5be2f141ef3585c7373f7f9c63be82007dac528e
Rajesh1st <rajeshtb55@outlook.com>
2025-03-23 12:02:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -2,6 +2,6 @@ import os
 
 API_ID = int(os.getenv("API_ID", "YOUR_API_ID"))
 API_HASH = os.getenv("API_HASH", "YOUR_API_HASH")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")
-ADMIN_ID = os.getenv("ADMIN_ID", "7252430326")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "YOUR_BOT_TOKEN")  # Fixed this line
+CHANNEL_ID = int(os.getenv("CHANNEL_ID", "-1002339454272"))  # Fixed the syntax
 MONGO_URI = os.getenv("MONGO_URI", "YOUR_MONGODB_CONNECTION_STRING")
