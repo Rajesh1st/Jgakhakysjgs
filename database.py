c16ef38ac28c87faa5b0eadad8dd63dc34943b66
Rajesh1st <rajeshtb55@outlook.com>
2025-03-23 11:58:07 +0530

    Update database.py

--- database.py
+++ database.py
@@ -1,38 +1,27 @@
 import json
 
-DB_FILE = "channels.json"
+DB_FILE = "files.json"
 
-# Load existing channels
-def load_channels():
+# Load existing file mappings
+def load_files():
     try:
         with open(DB_FILE, "r") as f:
             return json.load(f)
     except FileNotFoundError:
-        return []
+        return {}
 
-# Save channels
-def save_channels(channels):
+# Save file mappings
+def save_files(files):
     with open(DB_FILE, "w") as f:
-        json.dump(channels, f)
+        json.dump(files, f)
 
-# Add a channel
-def add_channel(channel_id):
-    channels = load_channels()
-    if channel_id not in channels:
-        channels.append(channel_id)
-        save_channels(channels)
-        return True
-    return False
+# Save a new file ID with message ID
+def save_file(file_id, message_id):
+    files = load_files()
+    files[file_id] = message_id
+    save_files(files)
 
-# Remove a channel
-def remove_channel(channel_id):
-    channels = load_channels()
-    if channel_id in channels:
-        channels.remove(channel_id)
-        save_channels(channels)
-        return True
-    return False
-
-# Get list of channels
-def get_channels():
-    return load_channels()
+# Retrieve message ID by file ID
+def get_file(file_id):
+    files = load_files()
+    return files.get(file_id, None)
